f9608d0bd9328845938c4d76d18e467faf4eea3c
Praveen Kumar S <78217016+Kanoro27@users.noreply.github.com>
2022-03-23 22:51:33 +0530

    code to calculate your height
    The Height must be Converted to both inches and feet.

---
+++ [004]heightconversion.py
@@ -0,0 +1,6 @@
+height=int(input("Enter your Height in Centimeters : "))
+hin=height/2.54 #height in inches is given as hin and 1 inch = 2.54 cm
+hf=hin/12 #height in foot is given as hf and 1 foot = 12 inches
+print("Height in Centinmeters : ",height)
+print("Height in Inches : ",hin)
+print("Height in Feet : ",hf)
