c38f2eab20722d1159bbdf3e7782f94f9231b9ba
Praveen Kumar S <78217016+Kanoro27@users.noreply.github.com>
2022-03-23 22:58:19 +0530

    Average of Marks
    Write a Python program that accepts marks in 5 subjects and outputs the average marks.

---
+++ [006]averageofmarks.py
@@ -0,0 +1,7 @@
+sub1=int(input("Enter the Marks for Subject-1 : "))
+sub2=int(input("Enter the Marks for Subject-2 : "))
+sub3=int(input("Enter the Marks for Subject-3 : "))
+sub4=int(input("Enter the Marks for Subject-4 : "))
+sub5=int(input("Enter the Marks for Subject-5 : "))
+avg=(sub1+sub2+sub3+sub4+sub5)/5.0
+print("The Average of the Student is ",avg)
