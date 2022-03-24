91bfe5493176f6d2295408acb5b0cce75af2e181
Praveen Kumar S <78217016+Kanoro27@users.noreply.github.com>
2022-03-24 20:17:26 +0530

    task3
    Write a Program to read details like name, class, age and then print the details first in same line and in seperate lines
    Make sure you have two blank lines between these two different type of print types

---
+++ [009]task3.py
@@ -0,0 +1,7 @@
+#code to represent different type of prints
+name=input("Enter Your Name : ")
+age=input("Enter the Student's Age : ")
+cla=input("Enter the Student's Class in Numbers : ")
+print(name,cla,age) #firzt type of print in same line
+print("\n") #shifting lines
+print(name,"\n",cla,"\n",age) #second type of print in different lines
